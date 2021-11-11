67c595f18ccffbbf41cef065232b6497eaebe8b0
skullkode <skullcode80@gmail.com>
2021-11-11 14:59:00 +0100

    First Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // prints "Hello, World!" to the terminal
+        System.out.println("Hello, World!");
+    } // end main
+
+} // end class
